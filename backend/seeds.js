65439e61b79e83023ba81b79a2fe42776dcab7ce
Daniel Bellmas <danielb@webiya.co.il>
2022-03-18 15:24:25 +0200

    Remove connection log + Add catch

--- backend/seeds.js
+++ backend/seeds.js
@@ -3,31 +3,27 @@ const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGODB_URI);
 
-const conSuccess = mongoose.connection;
-conSuccess.once("open", (...args) => {
-  console.log("Database connected:", args);
-});
+// seed database
+const seed = async () => {
+  for (let index = 0; index < 100; index++) {
+    await fetch("http://localhost:3000/api/items", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        authorization:
+          "Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMzQ3NmVlMTI0YWRlM2NkODI3NDYzNSIsInVzZXJuYW1lIjoiZGFuaWVsIiwiZXhwIjoxNjUyNzg1ODg2LCJpYXQiOjE2NDc2MDU0ODZ9.piBBE3GXONWFP6sk9-sPHUyXTOhneRDQgjvhfoBCp0k",
+      },
+      body: JSON.stringify({
+        item: {
+          title: `seed-${index}`,
+          description: "description description description",
+        },
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }
+};
 
-// // seed database
-// const seed = async () => {
-//   for (let index = 42; index < 100; index++) {
-//     await fetch("http://localhost:3000/api/items", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//         authorization:
-//           "Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMzQ3NmVlMTI0YWRlM2NkODI3NDYzNSIsInVzZXJuYW1lIjoiZGFuaWVsIiwiZXhwIjoxNjUyNzg1ODg2LCJpYXQiOjE2NDc2MDU0ODZ9.piBBE3GXONWFP6sk9-sPHUyXTOhneRDQgjvhfoBCp0k",
-//       },
-//       body: JSON.stringify({
-//         item: {
-//           title: `seed-${index}`,
-//           description: "description description description",
-//         },
-//       }),
-//     })
-//       .then((res) => res.json())
-//       .then((data) => console.log(data));
-//   }
-// };
-
-// seed();
+seed();
