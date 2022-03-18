df552fda1674c8843c59b96fd9ba7c5ab8c27acb
Daniel Bellmas <danielb@webiya.co.il>
2022-03-18 18:05:27 +0200

    Seed Users

--- backend/seeds.js
+++ backend/seeds.js
@@ -2,28 +2,46 @@ var mongoose = require("mongoose");
 require("./models/User");
 require("./models/Item");
 var Item = mongoose.model("Item");
+var User = mongoose.model("User");
 
 mongoose.connect("mongodb://127.0.0.1:27017");
 
-// seed database
-const seed = async () => {
-  for (let index = 0; index < 110; index++) {
-    const item = new Item({
-      title: `Item ${index}`,
-      description: `Item ${index} description`,
+// seed database with users
+const seedUsers = async () => {
+  for (let index = 0; index < 100; index++) {
+    const user = new User({
+      username: `user${index}`,
+      email: `email${Math.random()}@gmail.com`,
     });
-    await item.save().then(
-      (result) => {
-        console.log(result);
-        console.log(`Item ${index} saved`);
-      },
-      () => {
-        console.log(`Item ${index} not saved`);
-      }
-    );
+
+    await user.save().then((user) => {
+      console.log(`User ${user.username} created`);
+    });
   }
-  console.log("seed done");
+  console.log("Users seeding done");
   process.exit();
 };
+seedUsers();
 
-seed();
+// // seed database with items
+// const seedItems = async () => {
+//   for (let index = 0; index < 100; index++) {
+//     const item = new Item({
+//       title: `Item ${index}`,
+//       description: `Item ${index} description`,
+//     });
+//     await item.save().then(
+//       (result) => {
+//         console.log(result);
+//         console.log(`Item ${index} saved`);
+//       },
+//       () => {
+//         console.log(`Item ${index} not saved`);
+//       }
+//     );
+//   }
+//   console.log("seed done");
+//   process.exit();
+// };
+
+// seedItems();
